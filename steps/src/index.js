06faf00db87fa53ac0c78862974e366c9edeb1fb
V.Kapanadze <vako.kapa@gmail.com>
2025-11-23 16:04:45 +0400

    txt

--- steps/src/index.js
+++ steps/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import "./index.css";
 
 
 
@@ -10,7 +11,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  <App />
   </React.StrictMode>
 );
 
