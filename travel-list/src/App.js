9531f9f7ed3ce9aa7b453f0101457834f9e23955
V.Kapanadze <vako.kapa@gmail.com>
2025-11-27 22:41:50 +0400

    travel-list start5

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,6 +1,7 @@
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
 ];
 
 export default function App(){
@@ -31,7 +32,12 @@ function PackingList(){
 }
 function Item({item}){
   return <li>
-    {item.description}
+    <span>
+    {item.quantity} {item.description}
+    </span>
+    <button>❌</button>
+
+
   </li>
 }
 
