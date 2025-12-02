6835479acf747ad914a940472e1c2cfd5dde32d7
V.Kapanadze <vako.kapa@gmail.com>
2025-12-02 15:46:26 +0400

    test project2

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const colors = [
+  { id: 1, value: "red" },
+  { id: 2, value: "blue" },
+  { id: 3, value: "green" },
+  { id: 4, value: "yellow" },
+  { id: 5, value: "black" },
+  { id: 6, value: "purple" },
+
+];
+
+export default function App(){
+  return <div className="app">
+    <Screen/>
+    <UserOptions/>
+  </div>
 }
 
-export default App;
+function Screen(){
+  return <div className="screen">
+
+  </div>
+}
+
+
+function UserOptions(){
+  return <div className="buttons">
+    {colors.map(color => (
+    <button key={colors.id} style={{BackgroundColor: colors.value}}></button>))}
+  </div>
+}
