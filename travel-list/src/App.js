1803c0d0aae83ddf23000131983a141df828da14
V.Kapanadze <vako.kapa@gmail.com>
2025-12-05 14:30:55 +0400

    ss21

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -23,12 +23,19 @@ function Logo(){
 function Form(){
   const [description, setDescription] = useState("")
   const [quantity, setQuantity] = useState(1)
+  const [items, setItems] = useState([])
+  
+  function HandleAddItems(item){
+    setItems(items => [...items, item])
+  }
 
   function handleSubmit(e) {
   e.preventDefault()
   if(!description) return;
   const newItem = {description, quantity, packed : false, id: Date.now()}
   console.log(newItem)
+
+  HandleAddItems(newItem)
   
   setDescription("")
   setQuantity(1)
