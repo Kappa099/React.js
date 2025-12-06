ff32ba59709264735402d04f08222e75e1d2cbac
V.Kapanadze <vako.kapa@gmail.com>
2025-12-06 15:52:36 +0400

    next4

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -79,9 +79,17 @@ function Item({item, onDeleteItem, onToggleItem}){
 }
 
 function Stats({items}){
+  if(!items.length) return <p className="stats">
+    <em>Start adding some items to your packing list 🚀</em>
+  </p>
   const numItems = items.length
+  const numPacked = items.filter((item) => item.packed).length
+  const percentage = Math.round((numPacked / numItems) * 100)
   return <footer className="stats">
-    <em>🧳You have {numItems} items on your list, and you already packed X (X%)</em>
+    <em>
+      {percentage === 100 ? "You got everything ready to go ✈️" :
+      `🧳You have ${numItems} items on your list, and you already packed ${numPacked} ${percentage}%`}
+      </em>
   </footer>
 }
  
