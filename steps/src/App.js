701351d2e55b477d6ca1b236a251d5b7396be32a
V.Kapanadze <vako.kapa@gmail.com>
2025-12-12 13:51:11 +0400

    textxt

--- steps/src/App.js
+++ steps/src/App.js
@@ -36,12 +36,16 @@ export default function App(){
       <p className="message">Step {step}: {messages[step-1]}</p>
 
       <div className="buttons">
-        <button style={{backgroundColor : "#7950f2", color: "#FFF"}}
-        onClick={handlePrevious}>Previous</button>
+        <Button bgColor="#7950f2" textColor="#FFF" onClick={handlePrevious}
         <button style={{backgroundColor : "#7950f2", color: "#FFF"}}
         onClick={handleNext}>Next</button>
       </div>
     </div>
   )}
     </>
+}
+function button(textColor, bgColor, onClick, text){
+   return (<button style={{backgroundColor : bgColor, color: textColor}}
+        onClick={onClick}>{text}
+        </button>)
 }
